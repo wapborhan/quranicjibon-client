a38e7389149392987f16fc7462c8a3e144b5d871
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-08-30 20:52:00 +0600

    New UI

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import MainComponents from "./components/MainComponents";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./components/assets/stylesheet/styles.min.css";
-import "./components/assets/stylesheet/custom.css";
-import "./components/assets/fonts/font.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <MainComponents />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
