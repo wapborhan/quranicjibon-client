9e7e2173da3e63a03f6163f93f96c8b5d911b4b7
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-08-31 16:28:59 +0600

    Sidebar Functional add

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,7 @@
 import React, { Fragment } from "react";
 import { NavLink } from "react-router-dom";
-import {
-  FaQuran,
-  FaBookOpen,
-  FaBookReader,
-  FaBook,
-  FaCopy,
-  FaMoon,
-  FaShareAlt,
-} from "react-icons/fa";
-import { FcAbout } from "react-icons/fc";
+import routes from "./routes";
+import { FaQuran, FaInfoCircle } from "react-icons/fa";
 
 const Sidebar = (props) => {
   return (
@@ -26,35 +18,20 @@ const Sidebar = (props) => {
           <span className="logo_name">Islam BD</span>
         </div>
         <ul className="nav-links">
-          <li>
-            <NavLink to="/">
-              <i className="fas fa-exclamation-triangle text-white">
-                <FaBookOpen />
-              </i>
-              <span className="links_name">আল কুরআন (সুরা ক্রমে) </span>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/hadith">
-              <i className="fas fa-exclamation-triangle text-white">
-                <FaBookReader />
-              </i>
-              <span className="links_name">Income</span>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/expense">
-              <i className="fas fa-exclamation-triangle text-white">
-                <FaBook />
-              </i>
-              <span className="links_name">Expense</span>
-            </NavLink>
-          </li>
-
+          {routes.map((data, idx) => (
+            <li key={idx}>
+              <NavLink to={data.path}>
+                <i className="fas fa-exclamation-triangle text-white">
+                  {data.iconname}
+                </i>
+                <span className="links_name">{data.tittle} </span>
+              </NavLink>
+            </li>
+          ))}
           <li className="log_out">
             <NavLink to="/setting">
               <i className="fas fa-exclamation-triangle text-white">
-                <FcAbout />
+                <FaInfoCircle />
               </i>
               <span className="links_name">About</span>
             </NavLink>
--- src/components/routes.js
+++ src/components/routes.js
@@ -1,10 +1,62 @@
+import {
+  FaQuran,
+  FaBookOpen,
+  FaBookReader,
+  FaBook,
+  FaCopy,
+  FaStopwatch,
+  FaMoon,
+  FaShareAlt,
+} from "react-icons/fa";
+import { GiGoat } from "react-icons/gi";
+// Page Import
 import Sura from "../pages/sura";
 import Hadith from "../pages/hadith";
 // import HomeOne from "./HomeOne";
 
 const routes = [
-  { path: "/", component: <Sura /> },
-  { path: "/hadith", component: <Hadith /> },
+  {
+    path: "/",
+    tittle: "আল কুরআন (সুরা ক্রমে)",
+    iconname: <FaBookOpen />,
+    component: <Sura />,
+  },
+  {
+    path: "/hadith",
+    tittle: "সহীহ হাদিস সমুহ",
+    iconname: <FaBookReader />,
+    component: <Hadith />,
+  },
+  {
+    path: "/kalema",
+    tittle: "কালেমা",
+    iconname: <FaBook />,
+    component: <Hadith />,
+  },
+  {
+    path: "/dowa",
+    tittle: "দোয়া সমুহ",
+    iconname: <FaCopy />,
+    component: <Hadith />,
+  },
+  {
+    path: "/salat",
+    tittle: "নামাজ ",
+    iconname: <FaStopwatch />,
+    component: <Hadith />,
+  },
+  {
+    path: "/ramjan",
+    tittle: "রমজান ",
+    iconname: <FaMoon />,
+    component: <Hadith />,
+  },
+  {
+    path: "/kurbani",
+    tittle: "কুরবানি",
+    iconname: <GiGoat />,
+    component: <Hadith />,
+  },
 ];
 
 export default routes;
