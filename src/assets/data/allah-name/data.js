f8f397a7fa4e577319ea44baaa8735422d8605ed
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-09-01 20:24:57 +0600

    Data Add

--- src/assets/data/allah-name/data.js
+++ src/assets/data/allah-name/data.js
@@ -7,6 +7,33 @@ const ALLAH_NAMES = [
     name_bn: "আল্লাহ",
     path: "https://ninety-nine-names-of-allah.sgp1.cdn.digitaloceanspaces.com/a_01_allah.mp3",
   },
+  {
+    fojilot:
+      "প্রত্যেক নামাযের পর ১০০ বার পড়লে, ইনশাআল্লাহ্‌ তার অন্তর থেকে সব ধরনের কঠোরতা ও অলসতা দূর হয়ে যাবে।  ",
+    id: "2",
+    meaning_bn: "পরম দয়ালু",
+    name_ar: "الرحمن",
+    name_bn: "আর রাহমান",
+    path: "https://ninety-nine-names-of-allah.sgp1.cdn.digitaloceanspaces.com/a_02_ar_rahman.mp3",
+  },
+  {
+    fojilot:
+      "প্রত্যেক নামাযের পর ১০০ বার করে পাঠ করলে, ইনশাআল্লাহ্‌ পৃথিবীর সকল বিপদ আপদ থেকে নিরাপদ থাকবে।",
+    id: "3",
+    meaning_bn: "অতিশয়-মেহেরবান",
+    name_ar: "الرحيم",
+    name_bn: "আর-রহী'ম",
+    path: "https://ninety-nine-names-of-allah.sgp1.cdn.digitaloceanspaces.com/a_03_ar_rahim.mp3",
+  },
+  {
+    fojilot:
+      "ফযরের নামাজের পর অধিকহারে পাঠ করবে, আল্লাহ্‌ তায়ালা তাকে ধনবান করে দিবেন। ",
+    id: "4",
+    meaning_bn: "সর্বকর্তৃত্বময়",
+    name_ar: "الملك",
+    name_bn: "আল-মালিক",
+    path: "https://ninety-nine-names-of-allah.sgp1.cdn.digitaloceanspaces.com/a_04_al_malik.mp3",
+  },
 ];
 
 export default ALLAH_NAMES;
--- src/pages/allah-name/index.jsx
+++ src/pages/allah-name/index.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import ALLAH_NAMES from "../../assets/data/allah-name/data";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
 
 class index extends Component {
   constructor(props) {
@@ -16,10 +18,35 @@ class index extends Component {
           <div className="row">
             {this.state.AllahName.map((name) => {
               return (
-                <div className="col md-4">
-                  <div className="card">
+                <div className="col-md-4" key={name.id}>
+                  <div className="card mt-3">
                     <div className="card-body">
-                      <div className="ar">{name.name_bn}</div>
+                      <div className="d-flex justify-content-between">
+                        <div className="name">
+                          <div className="name-ar mb-2 text-start font-ar">
+                            {name.name_ar}
+                          </div>
+                          <div className="translate">
+                            উচ্চারণ - <strong>{name.name_bn}</strong>
+                          </div>
+                        </div>
+                        <div className="play">
+                          <AudioPlayer
+                            autoPlay
+                            src={name.path}
+                            // onPlay={(e) => console.log("onPlay")}
+                            showSkipControls={false}
+                            showJumpControls={false}
+                            showDownloadProgress={false}
+                            showFilledVolume={false}
+                            hasDefaultKeyBindings={false}
+                            autoPlayAfterSrcChange={false}
+                          />
+                        </div>
+                      </div>
+                      <div className="meaning">
+                        অর্থ - <strong>{name.meaning_bn}</strong>{" "}
+                      </div>
                     </div>
                   </div>
                 </div>
