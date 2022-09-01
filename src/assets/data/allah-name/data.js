c79f925e976d4e8c85f3869e5c918891e6196c23
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-09-01 18:26:02 +0600

    Allah Name Page Add

---
+++ src/assets/data/allah-name/data.js
@@ -0,0 +1,12 @@
+const ALLAH_NAMES = [
+  {
+    fojilot: "প্রত্যহ ১০০০ বার এই নামের যিকির করলে ঈমান দৃঢ় ও মযবুত হয়।  ",
+    id: "1",
+    meaning_bn: "আল্লাহ",
+    name_ar: "الله",
+    name_bn: "আল্লাহ",
+    path: "https://ninety-nine-names-of-allah.sgp1.cdn.digitaloceanspaces.com/a_01_allah.mp3",
+  },
+];
+
+export default ALLAH_NAMES;
--- src/pages/allah-name/index.jsx
+++ src/pages/allah-name/index.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
+import ALLAH_NAMES from "../../assets/data/allah-name/data";
 
-const index = () => {
-  return <div>index</div>;
-};
+class index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      AllahName: ALLAH_NAMES,
+    };
+  }
+  render() {
+    // console.log(this.state.AllahName);
+    return (
+      <div className="topbarpt">
+        <div className="container">
+          <div className="row">
+            {this.state.AllahName.map((name) => {
+              return (
+                <div className="col md-4">
+                  <div className="card">
+                    <div className="card-body">
+                      <div className="ar">{name.name_bn}</div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default index;
